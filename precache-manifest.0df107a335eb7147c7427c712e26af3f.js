7beac7c7d96c22b850439bcafe5aae7a30ba7acc
Oscar Rangel <r4ngel93@debian-DELL>
2019-12-29 00:11:45 -0600

    Updates

---
+++ precache-manifest.0df107a335eb7147c7427c712e26af3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01900c73bf40d848947425cbbd3d0b98",
+    "url": "/react-drum-machine/index.html"
+  },
+  {
+    "revision": "bf42c6a3f9dacd9f1cef",
+    "url": "/react-drum-machine/static/css/main.27ac62d2.chunk.css"
+  },
+  {
+    "revision": "31cc94d11f204c43e2b1",
+    "url": "/react-drum-machine/static/js/2.14835fad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-drum-machine/static/js/2.14835fad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf42c6a3f9dacd9f1cef",
+    "url": "/react-drum-machine/static/js/main.03bfa5e9.chunk.js"
+  },
+  {
+    "revision": "6002ff9936584443939d",
+    "url": "/react-drum-machine/static/js/runtime-main.5393f6ae.js"
+  }
+]);
